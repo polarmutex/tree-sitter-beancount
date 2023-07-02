ff7f3b590d2be9815a9f2a4435bbec0f1387b112
Brian Ryall <brian@brianryall.xyz>
2023-07-02 14:41:33 -0400

    chore: nix flake update / formatting

--- flake.nix
+++ flake.nix
@@ -13,101 +13,98 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
-    { } //
-    flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
+    {}
+    // flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          #overlays = [self.overlays.default];
+        };
+        tree-sitter-env = pkgs.stdenv.mkDerivation {
+          name = "tree-sitter-env";
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+          ];
+          dontUnpack = true;
+          dontBuild = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            makeWrapper \
+              ${pkgs.tree-sitter}/bin/tree-sitter \
+              $out/bin/tree-sitter \
+              --prefix PATH : "${with pkgs; lib.makeBinPath [stdenv.cc nodejs]}"
+          '';
+        };
+      in rec {
+        checks = {
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
 
-          tree-sitter-env = pkgs.stdenv.mkDerivation {
-            name = "tree-sitter-env";
+            hooks = {
+              clang-format = {
+                enable = true;
+                name = "clang-format";
+                entry = "${pkgs.clang-tools}/bin/clang-format -style=file -i";
+                types = ["text" "c"];
+                # I don't care for generated files' formatting
+                excludes = ["src/parser.c" "src/tree_sitter/parser.h"];
+                language = "system";
+              };
 
-            nativeBuildInputs = with pkgs; [
-              makeWrapper
-            ];
+              alejandra.enable = true;
 
-            dontUnpack = true;
+              tree-sitter = {
+                enable = true;
+                name = "tree-sitter tests";
+                entry = "${tree-sitter-env}/bin/tree-sitter test";
+                pass_filenames = false;
+              };
 
-            dontBuild = true;
-
-            installPhase = ''
-              mkdir -p $out/bin
-              makeWrapper \
-                ${pkgs.tree-sitter}/bin/tree-sitter \
-                $out/bin/tree-sitter \
-                --prefix PATH : "${with pkgs; lib.makeBinPath [stdenv.cc nodejs]}"
-            '';
-          };
-
-        in
-        rec {
-          checks = {
-            pre-commit = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-
-              hooks = {
-                clang-format = {
-                  enable = true;
-                  name = "clang-format";
-                  entry = "${pkgs.clang-tools}/bin/clang-format -style=file -i";
-                  types = [ "text" "c" ];
-                  # I don't care for generated files' formatting
-                  excludes = [ "src/parser.c" "src/tree_sitter/parser.h" ];
-                  language = "system";
-                };
-
-                nixpkgs-fmt = {
-                  enable = true;
-                };
-
-                tree-sitter = {
-                  enable = true;
-                  name = "tree-sitter tests";
-                  entry = "${tree-sitter-env}/bin/tree-sitter test";
-                  pass_filenames = false;
-                };
-
-                tree-sitter-files = {
-                  enable = true;
-                  name = "tree-sitter generated files";
-                  entry = "${tree-sitter-env}/bin/tree-sitter generate";
-                  pass_filenames = false;
-                };
+              tree-sitter-files = {
+                enable = true;
+                name = "tree-sitter generated files";
+                entry = "${tree-sitter-env}/bin/tree-sitter generate";
+                pass_filenames = false;
               };
             };
           };
+        };
 
-          devShells = {
-            default = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [
-                nodejs-16_x
-                python39
-                nodePackages.typescript
-                rustc
-                cargo
-                rustfmt
-                clippy
-                (tree-sitter.override { webUISupport = true; })
-              ];
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+              nodePackages.node-gyp
+              #python39
+              #nodePackages.typescript
+              #rustc
+              #cargo
+              #rustfmt
+              #clippy
+              (tree-sitter.overrideAttrs (_: {webUISupport = true;}))
+            ];
 
-              inherit (checks.pre-commit) shellHook;
-            };
+            inherit (checks.pre-commit) shellHook;
           };
+        };
 
-          packages = {
-            default = packages.tree-sitter-beancount;
+        packages = {
+          default = packages.tree-sitter-beancount;
 
-            inherit (pkgs.tree-sitter.passthru.builtGrammars) tree-sitter-beancount;
+          inherit (pkgs.tree-sitter.passthru.builtGrammars) tree-sitter-beancount;
 
-            inherit (pkgs) tree-sitter;
-          };
-        }
-      ) // {
+          inherit (pkgs) tree-sitter;
+        };
+      }
+    )
+    // {
       overlays = {
         default = final: prev: {
           tree-sitter = prev.tree-sitter.override {
