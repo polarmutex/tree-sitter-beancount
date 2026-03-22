8d17c9f976eca22b5250aa5536cb720e40de1ec2
GitHub Action <action@github.com>
2026-03-22 08:22:54 +0000

    chore: regenerate parser after dependency update

--- src/parser.c
+++ src/parser.c
@@ -17373,7 +17373,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_beancount(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 2,
-      .minor_version = 4,
+      .minor_version = 5,
       .patch_version = 1,
     },
   };
